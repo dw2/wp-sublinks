0f71aedcc4b76d79081ec7bf007234b4758ede04
Douglas Waltman II <doug@dougwaltman.com>
2011-08-29 10:25:35 -0700

    Updated Description

--- wp-sublinks.php
+++ wp-sublinks.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: wp-sublinks
 Plugin URI: http://dougwaltman.com/
-Description: Creates an unordered list of links for the sub-pages of the specified ID. If no ID is specified, the current page will be used instead. Examples: [subpage-linkage] (all sub-pages of the current page), [sublinks ID="2"] (all sub-pages of the page with the ID of 2), [sublinks ID="0"] (full sitemap)
+Description: Creates an unordered list of links for the sub-pages of the specified ID(s). If no ID is specified, the current page will be used instead. Examples: [sublinks] (all sub-pages of the current page), [sublinks of="2"] (all sub-pages of the page with the ID of 2), [sublinks of="0"] (full sitemap)
 Version: 3
 Author: Douglas Waltman II
 Author URI: http://dougwaltman.com/
